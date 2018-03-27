18dc1efac01bc5342929ef2ff8d713f1c4368ac9
shohei.k <s.kihara0316@gmail.com>
2018-03-27 16:19:06 +0900

    10.6

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,19 @@ class SessionsController < ApplicationController
 
   	user = User.find_by(email: params[:session][:email].downcase)
   	if user && user.authenticate(params[:session][:password])
-  	 log_in user
-     params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-  	 redirect_to user
-  	 flash[:prymary] = "You are now logged in"
+      if user.activated?
+    	 log_in user
+       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+  	   flash[:prymary] = "You are now logged in"
+       redirect_to user
   	else
-  		flash.now[:alert] = 'Invalid credentials, Please try again'
+      message = 'Account not activated!'
+      message += 'Check your email for an activation link'
+      flash[:alert] = message
+      redirect_to root_url
+    end
+  else
+  		flash.now[:alert] = 'Invalid credentials, Please try again!'
   		render 'new'
   	end
   end
